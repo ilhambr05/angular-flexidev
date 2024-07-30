60b6d52c86f279f20e9a147d23eaffea869d4827
Ilham Burhanuddin <ilham.r@wistkey.dev>
2024-07-30 17:15:49 +0700

    add basic routes + page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: AppComponent,
+    },
+    // { path: '', redirectTo: 'people', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent },
+];
